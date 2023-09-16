45a25dd580b85cb284c5389318dec85b61405eb1
Utkarsh Vaish <122693972+Utkarshvaish12@users.noreply.github.com>
2023-09-17 00:43:41 +0530

    Consecutive Numbers.sql

--- Medium/Consecutive Numbers.sql
+++ Medium/Consecutive Numbers.sql
@@ -1,1 +1,22 @@
+SELECT distinct 
+    i1.num as ConsecutiveNums 
+FROM 
+    logs i1,
+    logs i2,
+    logs i3
+WHERE 
+    i1.id=i2.id+1 AND 
+    i2.id=i3.id+1 AND 
+    i1.num=i2.num AND 
+    i2.num=i3.num
+/*
+    with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
 
+)
+
+select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
+*/
